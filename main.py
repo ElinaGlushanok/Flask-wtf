629284ef8bf70822dedaa462c93632b91e6655f4
Elina <elinaglushanok@gmail.com>
2024-03-21 22:53:25 +0300

    Answers added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import flask
+from flask import url_for
 
 app = flask.Flask(__name__)
 
@@ -10,6 +11,18 @@ def index(title=''):
 @app.route('/list_prof/<string:list>')
 def prof(list='ol'):
     return flask.render_template('prof.html', list=list)
+
+@app.route('/answer')
+@app.route('/auto_answer')
+def answer():
+    ans = ['Watny', 'Mark', 'выше среднего', 'штурман марсохода', 'male', 'всегда мечтал застрять на Марсе!', True]
+    answer = {'title': 'Ответы',
+           'surname': ['Фамилия', ans[0]], 'name': ['Имя', ans[1]],
+           'education': ['Образование', ans[2]], 'profession': ['Профессия', ans[3]],
+           'sex': ['Пол', ans[4]], 'motivation': ['Мотивация', ans[5]],
+           'ready' : ['Готовы остаться на Марсе?', ans[6]]}
+    return flask.render_template('auto_answer.html', answer=answer)
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
